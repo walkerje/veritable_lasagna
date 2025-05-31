440bad9103117c54bd0ed8720fb2f5adb69d43c1
walkerje <walkerje@berea.edu>
2025-05-31 00:36:51 -0400

    Update bash install script.

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ git clone https://github.com/walkerje/veritable_lasagna.git __vl_install_temp &&
   cd build &&
   cmake -DCMAKE_BUILD_TYPE=Release .. &&
   sudo cmake --build . --target install  &&
-  cd ../ &&
-  rm -r ./__vl_install_temp
+  cd ../../ &&
+  rm -rf ./__vl_install_temp
+  : ' rm -rf SCARY'
