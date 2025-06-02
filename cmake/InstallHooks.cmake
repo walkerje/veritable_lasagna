9bb304ebcad3ae8714223ad302d2ede90548d2bf
walkerje <walkerje@berea.edu>
2025-06-02 00:16:08 -0400

    docs: version bump

--- cmake/InstallHooks.cmake
+++ cmake/InstallHooks.cmake
@@ -3,12 +3,12 @@ file(MAKE_DIRECTORY "${CMAKE_SOURCE_DIR}/.git/hooks")
 
 # Copy hooks
 configure_file(
-        "${CMAKE_SOURCE_DIR}/cmake/hooks/pre-push"
-        "${CMAKE_SOURCE_DIR}/.git/hooks/pre-push"
+        "${CMAKE_SOURCE_DIR}/cmake/hooks/pre-commit"
+        "${CMAKE_SOURCE_DIR}/.git/hooks/pre-commit"
         COPYONLY
 )
 
 # Make hooks executable
-file(CHMOD "${CMAKE_SOURCE_DIR}/.git/hooks/pre-push"
+file(CHMOD "${CMAKE_SOURCE_DIR}/.git/hooks/pre-commit"
         PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE
 )
