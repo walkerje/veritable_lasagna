910fac2c1fd331b8a6f5ab5f2ebefda9a719eccf
walkerje <walkerje@berea.edu>
2025-06-02 00:44:51 -0400

    Modify version propagation git hook for more reliable results

--- cmake/InstallHooks.cmake
+++ cmake/InstallHooks.cmake
@@ -3,12 +3,12 @@ file(MAKE_DIRECTORY "${CMAKE_SOURCE_DIR}/.git/hooks")
 
 # Copy hooks
 configure_file(
-        "${CMAKE_SOURCE_DIR}/cmake/hooks/pre-commit"
-        "${CMAKE_SOURCE_DIR}/.git/hooks/pre-commit"
+        "${CMAKE_SOURCE_DIR}/cmake/hooks/pre-push"
+        "${CMAKE_SOURCE_DIR}/.git/hooks/pre-push"
         COPYONLY
 )
 
 # Make hooks executable
-file(CHMOD "${CMAKE_SOURCE_DIR}/.git/hooks/pre-commit"
+file(CHMOD "${CMAKE_SOURCE_DIR}/.git/hooks/pre-push"
         PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE
 )
